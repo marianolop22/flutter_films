d22b6d384829159ccb68b7f3ab971e63998dcee8
Mariano <marianolop22@yahoo.com.ar>
2019-06-21 07:54:26 -0300

    avance

--- lib/src/providers/films_provider.dart
+++ lib/src/providers/films_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter_films/src/models/film_model.dart';
@@ -9,6 +10,21 @@ class FilmsProvider {
   String _url = 'api.themoviedb.org';
   String _language = 'es-ES';
 
+
+  //Para el stream
+  int _popularPage = 0;
+  List<Film> _popularList = new List();
+  final _popularStreamController = StreamController<List<Film>>.broadcast(); //esto lo van a escuchar muchos
+
+
+  Function (List<Film>) get popularSink => _popularStreamController.sink.add;
+
+  Stream<List<Film>> get popularStream => _popularStreamController.stream;
+
+  void disposeStreams () {
+    _popularStreamController?.close(); //el signo de pregunta es por si no está inicializado
+
+  }
 
   Future <List<Film>> _proccessResponse (Uri url) async {
     final response = await http.get(url);
@@ -31,12 +47,21 @@ class FilmsProvider {
 
   Future <List<Film>>getPopular () async {
 
+    _popularPage++;
+
     final url = Uri.https(_url, '3/movie/popular', {
       'api_key':_apiKey,
-      'language':_language
+      'language':_language,
+      'page':_popularPage.toString()
     });
 
-    return await _proccessResponse(url);
+    final response = await _proccessResponse(url);
+
+    _popularList.addAll(response);
+
+    popularSink(_popularList);
+
+    return response;
   }
 
 
